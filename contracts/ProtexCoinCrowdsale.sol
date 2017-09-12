29594320cabf099f1431adc0e0cf6c95bf191679
Kieran Stolorz <kieranstolorz12@gmail.com>
2017-09-12 00:58:37 -0700

    added some Protex test code

---
+++ contracts/ProtexCoinCrowdsale.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.11;
+
+import './ProtexCoin.sol';
+import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
+
+
+contract ProtexCoinCrowdsale is Crowdsale {
+
+  function ProtexCoinCrowdsale(uint256 _startBlock, uint256 _endBlock, uint256 _rate, address _wallet) Crowdsale(_startBlock, _endBlock, _rate, _wallet) {
+  }
+
+  // creates the token to be sold.
+  // override this method to have crowdsale of a specific MintableToken token.
+  function createTokenContract() internal returns (MintableToken) {
+    return new ProtexCoin();
+  }
+
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,14 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+//var ConvertLib = artifacts.require("./ConvertLib.sol");
+//var MetaCoin = artifacts.require("./MetaCoin.sol");
+var ProtexCoinCrowdsale = artifacts.require("./ProtexCoinCrowdsale.sol")
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+
+module.exports = function(deployer, network, accounts) {
+	//TODO: set all of these variables properly, just place holders for now
+  const startBlock = web3.eth.blockNumber + 2
+  const endBlock = startBlock + 300
+  const rate = new web3.BigNumber(1000)
+  const wallet = web3.eth.accounts[0]
+
+  deployer.deploy(ProtexCoinCrowdsale, startBlock, endBlock, rate, wallet);
 };
